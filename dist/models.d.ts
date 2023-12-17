3f21ae9d28cc4ba2619f833338997f565c524dfb
justin26l <laichimeng2001@gmail.com>
2023-12-17 17:37:24 +0800

    update schema

--- dist/models.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function json2Mongoose(jsonSchema: {
-    [key: string]: any;
-}, interfacePath: string, schemaFileName: string): string;
-export declare function compileFromFile(jsonSchemaPath: string, modelToInterfacePath: string, outputPath: string): void;
